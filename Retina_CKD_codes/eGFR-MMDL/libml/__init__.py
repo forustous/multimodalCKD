5d49ffb2370dd9c6663191d9bddca6dcfe0efebd
YoungMin Bhak <ymb943@gmail.com>
2024-08-04 19:39:42 +0900

    upload inference code

--- Retina_CKD_codes/eGFR-MMDL/libml/__init__.py
+++
@@ -1,2 +0,0 @@
-from .utils import get_logger
-from .grad_cam import generate_grad_cam, getSmoothGradCAMPP
---
+++ models_ckd/libml/__init__.py
@@ -0,0 +1,2 @@
+from .utils import get_logger
+from .grad_cam import generate_grad_cam, getSmoothGradCAMPP
